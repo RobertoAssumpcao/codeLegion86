c6482d876fac8e70f6b2be240de4a15fde75d9d8
RobertoAssumpcao <betoassumpcao@outlook.com>
2023-12-31 19:25:29 -0300

    init

---
+++ CodeLegion86/ConsoleCodeLegion86/Program.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using System.Text;
+
+Console.WriteLine(@"
+░█████╗░░█████╗░██████╗░███████╗██╗░░░░░███████╗░██████╗░██╗░█████╗░███╗░░██╗░█████╗░░█████╗░
+██╔══██╗██╔══██╗██╔══██╗██╔════╝██║░░░░░██╔════╝██╔════╝░██║██╔══██╗████╗░██║██╔══██╗██╔═══╝░
+██║░░╚═╝██║░░██║██║░░██║█████╗░░██║░░░░░█████╗░░██║░░██╗░██║██║░░██║██╔██╗██║╚█████╔╝██████╗░
+██║░░██╗██║░░██║██║░░██║██╔══╝░░██║░░░░░██╔══╝░░██║░░╚██╗██║██║░░██║██║╚████║██╔══██╗██╔══██╗
+╚█████╔╝╚█████╔╝██████╔╝███████╗███████╗███████╗╚██████╔╝██║╚█████╔╝██║░╚███║╚█████╔╝╚█████╔╝
+░╚════╝░░╚════╝░╚═════╝░╚══════╝╚══════╝╚══════╝░╚═════╝░╚═╝░╚════╝░╚═╝░░╚══╝░╚════╝░░╚════╝░");
+Console.WriteLine("Digite o nome do usuário");
+string? nomeUsuario = Console.ReadLine();
+
+if (string.IsNullOrEmpty(nomeUsuario))
+{
+    Console.WriteLine("Nome Inválido");
+}
+else
+{
+    using HttpClient client = new();
+    string jsonRequest = JsonConvert.SerializeObject(new
+    {
+        query = $@"
+            {{
+                User(name: ""{nomeUsuario}"") {{
+                    id
+                    name
+                    about
+                    avatar {{
+                    large
+                    medium
+                    }}
+                }}
+            }}"
+    });
+
+    var httpRequest = new HttpRequestMessage
+    {
+        RequestUri = new Uri("https://graphql.anilist.co"),
+        Method = HttpMethod.Post,
+        Content = new StringContent(jsonRequest, Encoding.UTF8, "application/json")
+    };
+
+    HttpResponseMessage httpResponse = await client.SendAsync(httpRequest);
+    httpResponse.EnsureSuccessStatusCode();
+
+    string? jsonResponse = await httpResponse.Content.ReadAsStringAsync();
+
+    if (string.IsNullOrEmpty(jsonResponse))
+    {
+        Console.WriteLine("Erro interno.");
+    }
+    else
+    {
+        dynamic responseObject = JsonConvert.DeserializeObject(jsonResponse);
+        Console.WriteLine(responseObject);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,14 @@
+# AniList Profile Information Saver
+
+Este é um programa de console em C# que utiliza a API GraphQL da AniList para recuperar informações de perfil de usuário com base no nome de usuário inserido pelo terminal.
+
+## Pré-requisitos
+
+- .NET Core SDK 8.0 ou superior instalado
+
+## Como Usar
+
+1. **Clone o repositório:**
+
+   ```bash
+   git clone https://github.com/seu-usuario/nome-do-repositorio.git
